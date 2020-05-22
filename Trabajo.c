00ee2af87a0e95cd4c6fb2616d55108a68f1c608
JaviPalomeque <47509892+JaviPalomeque@users.noreply.github.com>
2020-05-22 18:39:49 +0200

    Update Trabajo.c

--- Trabajo.c
+++ Trabajo.c
@@ -13,8 +13,8 @@ struct TCredenciales{
 };
 
 struct TElementos{
-	char elemento[50];
-	char simbolo[2];
+	char nombre[50];
+	char simbolo[3];
 	int nAtomico;
 	float masaAtomica;
 	int periodo;
@@ -22,12 +22,14 @@ struct TElementos{
 };
 
 
-//declaración de funciones
+//declaraciÃ³n de funciones
 void dibujamenu();
-char tablaperiodica(struct TTabla clase[]);
-char Credenciales(struct TCredenciales vector[]);
-char elementos(struct TElementos elemento[]);
-
+void tablaperiodica(struct TTabla clase[]);
+void Credenciales(struct TCredenciales vector[]);
+void elementos(struct TElementos elemento[]);
+void verCredenciales(struct TCredenciales vector[]);
+void Cuestionario1();
+void Cuestionario2();
 
 
 int main(){
@@ -42,7 +44,7 @@ int main(){
 	
 	int i,nelemento = 0;
 	
-	int tabla, credenciales;
+	int tabla, credenciales, vercredenciales;
 	
 	FILE*pfichero1;
 	
@@ -67,7 +69,7 @@ int main(){
 		return 0;
 	}
 	
-	pfichero3 = fopen("Credenciales.txt","a");
+	pfichero3 = fopen("Credenciales.txt","a+");
 	
 	if(pfichero3 == NULL){
 		
@@ -80,18 +82,14 @@ int main(){
 		nelemento++;	
 	}
 	
-	i=0;
 	
-	while(fscanf(pfichero2, "%s" , elemento[i].elemento)!= EOF){
-			i++;
+	for(i=0;i<nelemento;i++){
+		
+		fscanf(pfichero2,"%s %s %d %f %d %d", elemento[i].nombre, elemento[i].simbolo, &elemento[i].nAtomico, &elemento[i].masaAtomica, &elemento[i].periodo, &elemento[i].grupo);
 	}
 	
-	printf("%d\n",i);
+	
 
-
-	
-	
-	
 	fclose(pfichero1);
 	
 	fclose(pfichero2);
@@ -99,38 +97,49 @@ int main(){
 	
 	dibujamenu();
 	
-	tabla = tablaperiodica(clase);	
+	tablaperiodica(clase);	
 		
 	
 	do{
-		printf("\nIntroduce una de las opciones del menu\n");
+		printf("\n\nIntroduce una de las opciones del menu (0 para ver las opciones)\n");
 		scanf( "%d" , &opcion );
 		switch(opcion){
-		
+			
+			case 0:
+				system("cls");
+				dibujamenu();
+			break;
+			
 			case 1:printf("Introduce tus credenciales.\n");
-				   credenciales = Credenciales(vector);
+				   Credenciales(vector);
 			break;
 		
-			case 2:printf("MIrar credenciales previamente introducidas.\n");
+			case 2:printf("Mirar credenciales previamente introducidas.\n");
+					verCredenciales(vector);
 			break;
 		
 			case 3:printf("Indica el numero de un elemento y te decimos informacion de el.\n");
-				   scanf("%d",&numero);
+					elementos(elemento);
 			break;
 		
-			case 4:printf("Introduce el nombre abreviado ( Ejemplo-->Sodio: Na).\n");
+			case 4:printf("Ha entrado al Cuestionario 1.\n");
+				   Cuestionario1();
 			break;
 		
-			case 5:printf("Gracias por haber usado nuestro programa.\n");
+			case 5:printf("Ha entrado al Cuestionario 2.\n");
+				   Cuestionario2();
+			break;	
+		
+			case 6:printf("Gracias por haber usado nuestro programa.\n");
 			break;
 		
-			default: printf("Por favor introduzca un valor entre 1 y 5.\n");
+			default: printf("Por favor introduzca un valor entre 1 y 6.\n");
 			
 			//
 		
 		}
 		
-	}while(opcion!=5);
+	}while(opcion!=6);
 	
 	
 	
@@ -151,18 +160,20 @@ void dibujamenu(){
 	
 	printf("\t1. Introduce tus credenciales.\n\n");
 	
-	printf("\t2. Descripcion breve de la tabla periodica.\n\n");
+	printf("\t2. Mirar credenciales previamente introducidas.\n\n");
 	
-	printf("\t3. Introduce el numero del elemento y te decimos grupo y periodo.\n\n");
+	printf("\t3. Introduce el numero del elemento y te decimos informacion sobre el.\n\n");
 	
-	printf("\t4. Introduce el nombre del elemento y te damos una breve descripcion.\n\n");
+	printf("\t4. Realizar Cuestionario 1.\n\n");
 	
-	printf("\t5. Cerrar el programa.\n\n");
+	printf("\t5. Realizar Cuestionario 2.\n\n");
+	
+	printf("\t6. Cerrar el programa.\n\n");
 	
 	printf("--------------------------------------------------------------------------------\n\n");
 }
 
-char tablaperiodica(struct TTabla clase[]){
+void tablaperiodica(struct TTabla clase[]){
 	
 	int k,nelemento=118,longitud;
 	
@@ -271,28 +282,273 @@ char tablaperiodica(struct TTabla clase[]){
 	printf("\n");	
 }
 
-char Credenciales(struct TCredenciales vector[]){
+void Credenciales(struct TCredenciales vector[]){
+	
+	system("cls");
 	
 	int personas,i;
 	
 	FILE*pfichero3;
 	
-	pfichero3 = fopen("Credenciales.txt","a");
+	pfichero3 = fopen("Credenciales.txt","a+");
 	
-	printf("Cuantas credenciales(personas) quieres meter:\n");
+	printf("Cuantas credenciales (personas) quieres meter:\n");
 	scanf("%d",&personas);
 	
 	for(i=0 ; i<personas ; i++){
 		
+		printf("Nombre Apellido:\n");
 		fflush(stdin);
 		scanf("%s %s",vector[i].nombre, vector[i].apellidos);
 		fprintf( pfichero3, "%s	%s\n", vector[i].nombre, vector[i].apellidos );
 		
 	}
 	
+	fclose(pfichero3);
 	//return 0;
 }
 
-char elementos(struct TElementos elemento[]){
+void elementos(struct TElementos elemento[]){
 	
+	system("cls");
+	int n;
+	char seguir;
+	
+	do{
+		printf("Introduce el numero atomico del elemento:\n");
+		fflush(stdin);
+		scanf("%d",&n);
+		if (n<=0||n>118){
+			printf("No existe ese elemento\n");
+		}
+		
+		if (n>0&&n<=118){
+			printf("\nElemento: %s\nSimbolo: %s\nNAtomico: %d\nMasaAtomica: %.3f u\nPeriodo: %d\nGrupo: %d\n",elemento[n-1].nombre,elemento[n-1].simbolo,elemento[n-1].nAtomico,elemento[n-1].masaAtomica,elemento[n-1].periodo,elemento[n-1].grupo);
+		}
+		
+		printf("\nQuieres informacion sobre otro elemento?(s o n)\n");
+		fflush(stdin);
+		scanf("%c",&seguir);
+	}while(seguir=='s');	
+	
+}
+
+
+void verCredenciales(struct TCredenciales vector[]){
+	
+	system("cls");
+	int i, j=0;
+	FILE*pfichero3;
+	
+	
+	pfichero3 = fopen("Credenciales.txt","a+");
+	
+	
+	while(fscanf(pfichero3, "%s %s\n" , vector[j].nombre, vector[j].apellidos) != EOF){
+		
+		j++;	
+		
+	}
+	
+	for(i=0 ; i<j ; i++){
+		fflush(stdin);
+		printf("%d.- %s %s\n", i+1, vector[i].nombre, vector[i].apellidos);
+	}
+	
+}
+
+void Cuestionario1(){
+	
+	char simbolo ,platino[5], platinosol[5]= "Pt", fluor, plata[5], platasol[5]= "Ag",  rodio[5], rodiosol[5]= "Rh", wolframio ;
+	int resultado, contAciertos=0, contErrores=0, errores=0, aciertos=0, nota;
+	
+	system("cls");
+	
+	printf("___PRIMER CUESTIONARIO___\n\n");
+	
+	printf("NOTA: Las respuetas correctas sumaran 2 puntos y las erroneas restaran 1 punto \n\n");
+	
+	
+	//1PREGUNNTA
+	printf("Que simbolo tiene el elemento quimico platino \n\n");
+	fflush(stdin);
+	gets(platino);
+    
+    resultado = strcmp(platino, platinosol);
+	if (resultado == 0) {
+		printf("VERDADERO\n");
+		contAciertos+=2;
+		aciertos++;
+	} else {
+		printf("FALSO\n");
+		contErrores++;
+		errores++;
+	}
+    
+    
+    //2PREGUNNTA
+    printf("Que simbolo tiene el elemento quimico fluor \n\n");
+    fflush(stdin);
+	scanf("%c",&fluor);
+	
+	if (fluor == 'F') {
+		printf("VERDADERO\n");
+		contAciertos+=2;
+		aciertos++;
+	} else {
+		printf("FALSO\n");
+		contErrores++;
+		errores++;
+	}
+	
+	fflush(stdin);
+	
+    //3PREGUNNTA
+    printf("Que simbolo tiene el elemento quimico plata \n\n");
+    fflush(stdin);
+	gets(plata);
+    
+    resultado = strcmp(plata, platasol);
+	if (resultado == 0) {
+		printf("VERDADERO\n");
+		contAciertos+=2;
+		aciertos++;
+	} else {
+		printf("FALSO\n");
+		contErrores++;
+		errores++;
+	}
+	
+	//4PREGUNNTA
+	printf("Que simbolo tiene el elemento quimico rodio \n\n");
+	fflush(stdin);
+	gets(rodio);
+    
+    resultado = strcmp(rodio, rodiosol);
+	if (resultado == 0) {
+		printf("VERDADERO\n");
+		contAciertos+=2;
+		aciertos++;
+	} else {
+		printf("FALSO\n");
+		contErrores++;
+		errores++;
+	}
+	
+	//5PREGUNNTA
+	printf("Que simbolo tiene el elemento quimico wolframio \n\n");
+	fflush(stdin);
+	scanf("%c",&wolframio);
+	
+	if (wolframio == 'W') {
+		printf("VERDADERO\n");
+		contAciertos+=2;
+		aciertos++;
+	} else {
+		printf("FALSO\n");
+		contErrores++;
+		errores++;
+	}
+    
+    printf("Tus respuestas acertadas han sido %i\n\n", aciertos);
+    printf("Tus respuestas erroneas han sido %i\n\n", errores);
+    
+	nota=contAciertos-contErrores;
+	if (nota<0){
+		nota=0;
+	}
+    printf("Tu calificacion es: %i", nota);
+}
+
+void Cuestionario2(){
+	
+	char simbolo;
+	int h, ne, bi, cl, ba, contAciertos=0, contErrores=0, errores=0, aciertos=0, nota;
+
+	system("cls");
+	
+	printf("___SEGUNDO CUESTIONARIO___\n\n");
+	
+	printf("NOTA: Las respuetas correctas sumaran 2 puntos y las erroneas restaran 1 punto \n\n");
+	
+	
+	//1PREGUNNTA
+	printf("Escribe cual es el numero atomico del Hidrogeno \n\n");
+	scanf("%i",&h);
+    
+	if (h == 1) {
+		printf("VERDADERO\n");
+		contAciertos+=2;
+		aciertos++;
+	} else {
+		printf("FALSO\n");
+		contErrores++;
+		errores++;
+	}
+    
+    //2PREGUNNTA
+	printf("Escribe cual es el numero atomico del Neon \n\n");
+	scanf("%i",&ne);
+    
+	if(ne == 10){
+		printf("VERDADERO\n");
+		contAciertos+=2;
+		aciertos++;
+	} else {
+		printf("FALSO\n");
+		contErrores++;
+		errores++;
+	}
+	
+	//3PREGUNNTA
+	printf("Escribe cual es el numero atomico del Bismuto \n\n");
+	scanf("%i",&bi);
+    
+	if (bi == 83) {
+		printf("VERDADERO\n");
+		contAciertos+=2;
+		aciertos++;
+	} else {
+		printf("FALSO\n");
+		contErrores++;
+		errores++;
+	}
+	
+	//4PREGUNNTA
+	printf("Escribe cual es el numero atomico del Cloro \n\n");
+	scanf("%i",&cl);
+    
+	if (cl == 17){
+		printf("VERDADERO\n");
+		contAciertos+=2;
+		aciertos++;
+	} else {
+		printf("FALSO\n");
+		contErrores++;
+		errores++;
+	}
+	
+	//5PREGUNNTA
+	printf("Escribe cual es el numero atomico del Bario \n\n");
+	scanf("%i",&ba);
+    
+	if (ba == 56) {
+		printf("VERDADERO\n");
+		contAciertos+=2;
+		aciertos++;
+	} else {
+		printf("FALSO\n");
+		contErrores++;
+		errores++;
+	}
+    
+    printf("Tus respuestas acertadas han sido %i\n\n", aciertos);
+    printf("Tus respuestas erroneas han sido %i\n\n", errores);
+    
+	nota=contAciertos-contErrores;
+	if (nota<0){
+		nota=0;
+	}
+    printf("Tu calificacion es: %i", nota);
+    
 }
